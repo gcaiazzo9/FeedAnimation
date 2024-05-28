c842324de12180c6f4055e794215722e295d6aa8
gcaiazzo9 <giorgiocaiazzo@icloud.com>
2024-05-28 14:36:57 +0200

    Initial Commit

---
+++ FeedAnimation/FeedAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedAnimationApp.swift
+//  FeedAnimation
+//
+//  Created by Giorgio Caiazzo on 28/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FeedAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
